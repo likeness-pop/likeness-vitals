34bd7c49ff42bcfd27fbb864efe7b78b037b0d07
Joe <tuccillojv@ornl.gov>
2025-03-20 14:27:09 -0400

    add .gitignore/.ci

--- likeness_vitals/__init__.py
+++ likeness_vitals/__init__.py
@@ -9,22 +9,22 @@ import contextlib
 import warnings
 from importlib.metadata import PackageNotFoundError, version
 
-warnings.warn(
-    (
-        "The ``likeness-vitals`` package is transitioning to a modular API for a "
-        "cleaner top-level namespace. Classes, functions, and attributes that "
-        "are currently available in the top-level will only be available within "
-        "their home modules. Both the current and the updated API will be in place "
-        "for at least two minor releases following the inclusion of this warning. "
-        "The release at the time of this warning inclusion was ``v1.3.0``, so "
-        "the current API will be stable until ``v1.5.0`` at the soonest, and "
-        "the ``v2.0.0`` release will be the absolute deadline. "
-        "See [https://code.ornl.gov/likeness/likeness-vitals/-/issues/32] "
-        "for details on how to update dependant packages."
-    ),
-    FutureWarning,
-    stacklevel=1,
-)
+# warnings.warn(
+#     (
+#         "The ``likeness-vitals`` package is transitioning to a modular API for a "
+#         "cleaner top-level namespace. Classes, functions, and attributes that "
+#         "are currently available in the top-level will only be available within "
+#         "their home modules. Both the current and the updated API will be in place "
+#         "for at least two minor releases following the inclusion of this warning. "
+#         "The release at the time of this warning inclusion was ``v1.3.0``, so "
+#         "the current API will be stable until ``v1.5.0`` at the soonest, and "
+#         "the ``v2.0.0`` release will be the absolute deadline. "
+#         "See [https://code.ornl.gov/likeness/likeness-vitals/-/issues/32] "
+#         "for details on how to update dependant packages."
+#     ),
+#     FutureWarning,
+#     stacklevel=1,
+# )
 
 
 from . import constants, sg_ops, vitals
